f841320aa4c1d4a5ef28d1f8efb1c9b321d338b5
liikii <609957891@qq.com>
2020-07-08 17:19:41 +0800

    u

--- tstmovie.py
+++ tstmovie.py
@@ -19,7 +19,7 @@ clips = [ImageClip('static/video_cover_%s.jpg' % i).set_duration(4) for i in ran
 slided_clips = [CompositeVideoClip([clip.fx(slide_in, duration=1, side=choice(drc))]) for clip in clips]
 slided_clips.insert(0, img0_n)
 final_clip = concatenate_videoclips(slided_clips, method="compose", padding=-1)
-music = AudioFileClip("static//aud1593779359532480.m4a")
+music = AudioFileClip("static/aud1593779359532480.m4a")
 audio = audio_loop(music, duration=final_clip.duration)
 final_clip = final_clip.set_audio(audio)
 final_clip.write_videofile("my_concatenation.mp4", fps=26, audio_bitrate="1000k", bitrate="4000k", codec='libx264', audio_codec='aac')
